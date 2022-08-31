df7725baf2c39538568896bbdf9a69c439c0048d
苍南 <914067438@qq.com>
2022-08-31 16:14:06 +0800

    fix: 格式化

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,13 +3,11 @@ import { DatabaseModule } from '@/common/database/database.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserProviders } from './user.providers';
-import { FeishuController } from './feishu/feishu.controller';
-import { FeishuService } from './feishu/feishu.service';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [FeishuController, UserController],
-  providers: [...UserProviders, UserService, FeishuService],
+  controllers: [UserController],
+  providers: [...UserProviders, UserService],
   exports: [UserService],
 })
 export class UserModule {}
